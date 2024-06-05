9927ed1b9d8f777298de471be6b3d5a74a108255
Ramdeniakki <ramdeniakki35@gmail.com>
2024-06-05 12:58:00 +0530

    Updating Dom

---
+++ DOM/script.js
@@ -0,0 +1,9 @@
+// console.log('hello')
+// window.console.log('hello world')
+
+let header = document.getElementById('heading') // h1
+
+let headers = document.getElementsByClassName('header-class');
+console.dir(headers)
+
+console.dir(header)
